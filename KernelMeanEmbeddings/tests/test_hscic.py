ef9403a9f1d4288038e693171a452e2d2b4b3b84
Simon Perschel <simon.perschel@t-online.de>
2021-05-08 17:14:58 +0200

    test added

--- KernelMeanEmbeddings/tests/test_hscic.py
+++ KernelMeanEmbeddings/tests/test_hscic.py
@@ -8,7 +8,7 @@ Y = np.random.normal(size = 500) * 0.3
 
 Z_2  = np.random.normal(size=500)
 X_2 = np.exp(-0.5*Z_2**2) * np.sin(2*Z_2) + np.random.normal(size=500) * 0.3
-Y_dep = np.exp(-0.5*Z_2**2) * np.sin(2*Z_2) + np.random.normal(size=500) * 0.3 + 0.2*X
+Y_dep = np.exp(-0.5*Z_2**2) * np.sin(2*Z_2) + np.random.normal(size=500) * 0.3 + 0.2*X_2
 
 Z_3  = np.random.normal(size=500)
 X_3 = np.exp(-0.5*Z_3**2) * np.sin(2*Z_3) + np.random.normal(size=500) * 0.3
@@ -26,4 +26,5 @@ xp = np.linspace(-2, 2, 200)
 plt.plot(xp, hscic(xp))
 plt.plot(xp, hscic_2(xp))
 plt.plot(xp, hscic_3(xp))
+plt.legend(["HSCIC(X, Y, Z, )", "HSCIC(X, Y_dep, Z)", "HSCIC(X, Yp_dep, Z)"])
 plt.show()
